2d8a8560ae0d92e9c02c68e0953a08c93fbf5a22
Dan Thomas <dan@ideacrew.com>
2018-11-05 08:00:22 -0500

    Add Bootstrap and debugging tools

--- app/models/parties/organization_party.rb
+++ app/models/parties/organization_party.rb
@@ -2,7 +2,9 @@ module Parties
 	class OrganizationParty < Party
 
 	  field :organization_id, type: String
-	  field :legal_name, 			type: String
+	  field :name, 						type: String
+	  field :dba, 						type: String
+	  field :fein,						type: String
 	  
 	end
 end
--- app/models/transactions/transaction_kind.rb
+++ app/models/transactions/transaction_kind.rb
@@ -1,43 +1,49 @@
-class Transactions::TransactionKind
-  include Mongoid::Document
-  include Mongoid::Timestamps
+module Transactions
+  TRANSACTION_KINDS = []
 
-	  embedded_in :transaction,
-	  						class_name: "Transactions::Transaction"
-
-	  field :key,										type: Symbol
-	  field :title, 									type: String
-	  field :description, 					type: String
+	class TransactionKind
+	  include Mongoid::Document
+	  include Mongoid::Timestamps
 
 
-	  private
+		  embedded_in :transaction,
+		  						class_name: "Transactions::Transaction"
+
+		  field :key,										type: Symbol
+		  field :title, 									type: String
+		  field :description, 					type: String
 
 
-	  TRANSACTION_KINDS = []
-
-	  def konstant(kind_class)
-	  	module_name = kind_class.to_s.deconstantize
-  		"#{module_name}::#{module_name.singularize.upcase}_KINDS"
-	  end
-
-	  def initialize_kind(kind_class)
-	  	list = [ 
-		  		add_enrollment: { title: "Add Enrollment", description: "" } 
-		  		]
-	  	list.each { kind_class. }
-	  end
-
-	  def load_kinds(kind_class)
-	  end
-
-	  def add_kind(kind_class, kind)
-	  end
-
-	  def update_kind(kind_class, kind)
-	  end
-
-	  def delete_kind(kind_class, kind)
-	  end
+		  # private
 
 
+
+		  def kind_konstant(kind_class)
+		  	module_name = kind_class.to_s.deconstantize
+	  		"#{module_name}::#{module_name.singularize.upcase}_KINDS".constantize
+		  end
+
+		  def initialize_kind(kind_class)
+		  	list = [ 
+			  			add_enrollment: { title: "Add Enrollment", description: "" },
+			  			terminate_enrollment: { title: "Terminate Enrollment", description: "" },
+			  		]
+
+			  binding.pry
+		  	kind_konstant = list.reduce([]) { |kind_list, kind| kind_list << kind }
+		  end
+
+		  def load_kinds(kind_class)
+		  end
+
+		  def add_kind(kind_class, kind)
+		  end
+
+		  def update_kind(kind_class, kind)
+		  end
+
+		  def delete_kind(kind_class, kind)
+		  end
+
+	end
 end
